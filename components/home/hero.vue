744bdf9b13017f6ea94dbdfec6c175bfb6dbe110
Félix Bouveret <bouveret.felix@gmail.com>
2020-02-08 21:42:48 +0100

    Add hero component

---
+++ components/home/hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="hero">
+    <div class="inner">
+      <h2 class="subtitle">
+        Here&#39;s my main nuxtJs project where I&#39;ll try different things
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
